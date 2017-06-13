dab5e68bd10e177e78dd8bbf0f1fc660f876dd70
Benjamin Franzke <benjaminfranzke@googlemail.com>
2017-06-13 19:17:08 +0200

    Fix the original babel example

--- bootstrap-custom.js
+++ bootstrap-custom.js
@@ -2,20 +2,30 @@
 
 // This is needed until bootstrap does no longer require jquery as global
 global.jQuery = require('jquery');
-//window.jQuery = require('jquery');
 
-var util = require('bootstrap/js/src/util');
+//var util = require('bootstrap/js/src/util');
 //var util = require('bootstrap-util');
-console.log(util);
+//console.log(util);
+
 //require('bootstrap/js/src/alert');
+
 //require('bootstrap/js/src/button');
+
 require('bootstrap/js/src/carousel');
-//require('bootstrap-carousel');
 
 //require('bootstrap/js/src/collapse');
+
+//global.Popper = require('popper.js');
 //require('bootstrap/js/src/dropdown');
+
 //require('bootstrap/js/src/modal');
+
 //require('bootstrap/js/src/scrollspy');
+
 //require('bootstrap/js/src/tab');
+
+//global.Popper = require('popper.js');
 //require('bootstrap/js/src/tooltip');
-//require('bootstrap/js/src/popover');
+
+global.Popper = require('popper.js');
+require('bootstrap/js/src/popover');

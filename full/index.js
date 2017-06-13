246345fe6e81e38f6a0eecc599d316b2e454dd0f
Benjamin Franzke <benjaminfranzke@googlemail.com>
2017-06-13 19:13:09 +0200

    Add an example for a full bootstrap browserify build, without shimming/babel

---
+++ full/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// jQuery and Popper are required to be available globally by bootstrap
+global.jQuery = require('jquery');
+global.Popper = require('popper.js');
+
+require('bootstrap');

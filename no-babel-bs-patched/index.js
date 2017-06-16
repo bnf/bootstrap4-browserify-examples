d04fad2be4f3415ecc97567956b4dfa8672d591b
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-06-16 21:29:54 +0200

    Fix no-babel-bs-patched example
    The bootstrap bundle (dist/js/bootstrap)
    requires jQuery and Popper to be globals.

--- no-babel-bs-patched/index.js
+++ no-babel-bs-patched/index.js
@@ -1,5 +1,9 @@
 "use strict";
-require('bootstrap')
 var $ = require('jquery')
+var Popper = require('popper.js')
+
+global.jQuery = $
+global.Popper = popper
+require('bootstrap')
 
 $('[data-toggle="popover"]').popover()
